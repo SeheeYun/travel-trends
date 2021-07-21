41370fe0108f8d4842915b30e83a258f39d727db
sehee yun <seheeyun29@gmail.com>
2021-07-22 00:15:33 +0900

    ➕ material-ui/core 추가

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,19 @@
+import axios from 'axios';
 import Head from 'next/head';
 import Image from 'next/image';
 import styles from '../styles/Home.module.css';
+import chartData from '../src/data/data.json';
 
-export default function Home() {}
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>국내 여행 트렌드</title>
+        <meta
+          name="description"
+          content="국내 여행 트렌드에 대한 데이터 시각화 사이트입니다."
+        />
+      </Head>
+    </div>
+  );
+}
